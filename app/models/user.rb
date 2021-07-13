b7cb2c4f6eb6d5233ab960039cf600b3cf82e418
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-07-13 14:46:39 +0000

    フォロー機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,24 @@ class User < ApplicationRecord
 
   has_many :diaries
   attachment :profile_image
+  
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  
+  # ユーザーをフォローする
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしていれば、trueを返す
+  def following?(user)
+    followings.include?(user)
+  end
 
   validates :user_name, presence: true, length: { in: 2..20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
