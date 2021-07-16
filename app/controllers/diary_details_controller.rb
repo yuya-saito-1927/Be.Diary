1b4393a865d93edfa8a9310bd27cb7e139a718e5
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-07-16 17:23:34 +0000

    日記詳細作成機能の実装

--- app/controllers/diary_details_controller.rb
+++ app/controllers/diary_details_controller.rb
@@ -1,8 +1,18 @@
 class DiaryDetailsController < ApplicationController
   def new
+    @diary_detail = DiaryDetail.new
   end
 
   def create
+    @diary = Diary.find(params[:diary_id])
+    @diary_detail = DiaryDetail.new(diary_detail_params)
+    @diary_detail.diary_id = @diary.id
+    if
+      @diary_detail.save
+      redirect_to diary_path(@diary.id), notice: "日記内容が作成されました"
+    else
+      render :new
+    end
   end
 
   def index
@@ -19,8 +29,15 @@ class DiaryDetailsController < ApplicationController
 
   def destroy
   end
-  
-  def destroy_all 
-  end 
-  
+
+  def destroy_all
+  end
+
+  private
+
+  def diary_detail_params
+    params.require(:diary_detail).permit(:title, :diary_detail_image, :action, :keep, :problem, :improve, :mind, :free)
+  end
+
+
 end
