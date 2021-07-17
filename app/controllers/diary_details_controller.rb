e4df190f09665f77629f3e1b014a3fe6ff137c0a
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-07-17 13:48:43 +0000

    日記内容の編集と削除機能の実装

--- app/controllers/diary_details_controller.rb
+++ app/controllers/diary_details_controller.rb
@@ -9,28 +9,39 @@ class DiaryDetailsController < ApplicationController
     @diary_detail.diary_id = @diary.id
     if
       @diary_detail.save
-      redirect_to diary_path(@diary.id), notice: "日記内容が作成されました"
+      redirect_to diary_diary_detail_path(@diary, @diary_detail.id), notice: "日記内容が作成されました"
     else
       render :new
     end
   end
 
-  def index
-  end
-
   def show
+    @diary_detail = DiaryDetail.find(params[:id])
   end
 
   def edit
+    @diary = Diary.find(params[:diary_id])
+    @diary_detail = DiaryDetail.find(params[:id])
   end
 
   def update
+    @diary = Diary.find(params[:diary_id])
+    @diary_detail = DiaryDetail.find(params[:id])
+    @diary_detail.diary_id = @diary.id
+    if
+      @diary_detail.update(diary_detail_params)
+      redirect_to diary_diary_detail_path(@diary, @diary_detail.id), notice: "日記内容が更新されました"
+    else
+      render :edit
+    end
   end
 
   def destroy
-  end
-
-  def destroy_all
+    @diary = Diary.find(params[:diary_id])
+    @diary_detail = DiaryDetail.find(params[:id])
+    @diary_detail.diary_id = @diary.id
+    @diary_detail.destroy
+    redirect_to diary_path(@diary.id), notice: "日記内容が削除されました"
   end
 
   private
