6bc194a49678f51a040d336e9c01aedf58b3db64
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-07-19 15:49:12 +0000

    非同期通信

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,7 +5,6 @@ class FavoritesController < ApplicationController
     @diary_detail = DiaryDetail.find(params[:diary_detail_id])
     favorite = current_user.favorites.new(diary_detail_id: @diary_detail.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
@@ -13,7 +12,6 @@ class FavoritesController < ApplicationController
     @diary_detail = DiaryDetail.find(params[:diary_detail_id])
     favorite = current_user.favorites.find_by(diary_detail_id: @diary_detail.id)
     favorite.destroy
-    redirect_to request.referer
   end
 
 end
