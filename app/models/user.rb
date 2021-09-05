3fa8094c628f2b59a04bef4b3ffea3e48e11e5e6
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-09-05 13:00:38 +0000

    Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  #
+  #gem deviseの導入
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   def active_for_authentication?
     super && (self.is_deleted == false)
   end
-
+  
+  #アソシエーションの設定
   has_many :diaries
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   attachment :profile_image
 
+  #フォローとフォロワーのアソシエーション
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
@@ -31,7 +33,8 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
-
+  
+  #バリデーションの設定
   validates :user_name, presence: true, length: { in: 2..20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
