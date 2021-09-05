30d3aeb84b1e5fbdc23250b99c33c91e3fe42066
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-09-05 12:55:29 +0000

    Update

--- app/models/diary_detail.rb
+++ app/models/diary_detail.rb
@@ -1,14 +1,19 @@
 class DiaryDetail < ApplicationRecord
   
+  #アソシエーション設定
   belongs_to :diary
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  
+  #イメージファイルの導入
   attachment :diary_detail_image
   
+  #バリデーション設定
   validates :title, presence: true, length: { in: 2..20 }
   validates :action, presence: true
   validates :mind, presence: true
   
+  #あるユーザーが特定の投稿をいいねしているかどうかの確認
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,6 @@
 class Favorite < ApplicationRecord
   
+  #アソシエーション設定
   belongs_to :user
   belongs_to :diary_detail
   
