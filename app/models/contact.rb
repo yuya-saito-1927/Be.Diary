8d151383ac043b75c9e7bc591e924c3125083f00
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-09-05 12:52:31 +0000

    Update

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,9 @@
 class Contact < ApplicationRecord
 
+  #enumによる選択肢の処理登録
   enum subject: { "会員等について": 0, "日記について": 1, "日記内容について": 2, "コメントについて": 3, "その他のお問い合わせ": 4 }
 
+  #バリテーション設定
   validates :name, presence: true
   validates :email, presence: true
   validates :phone_number, presence: true
--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,11 +1,16 @@
 class Diary < ApplicationRecord
   
+  #アソシエーション設定
   belongs_to :user
   has_many :diary_details
+  
+  #イメージファイルの導入
   attachment :diary_image
   
+  #タグつけ機能の導入
   acts_as_taggable
   
+  #バリデーション
   validates :title, presence: true, length: { in: 2..20 }
   validates :introduction, presence: true, length: { maximum: 100 }
   
