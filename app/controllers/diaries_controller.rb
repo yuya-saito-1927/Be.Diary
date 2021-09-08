1938b0051cf988ad582d74d7141cc93e504bf261
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-09-08 14:45:36 +0000

    Update

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,8 +1,11 @@
 class DiariesController < ApplicationController
+  
+  #新規日記作成画面
   def new
     @diary = Diary.new
   end
-
+  
+  #新規日記作成処理
   def create
     @diary = Diary.new(diary_params)
     @diary.user_id = current_user.id
@@ -13,26 +16,33 @@ class DiariesController < ApplicationController
       render :new
     end
   end
-
+  
+  #日記一覧画面
   def index
     @user = current_user
     @diaries_side = @user.diaries.reverse_order
+    #ページネーション機能（1ページにつき８個の設定）
     @diaries = Diary.all.page(params[:page]).per(8).reverse_order
+    #タグ機能、タグ検索による日記一覧の表示
     if params[:tag_name]
       @diaries = Diary.tagged_with("#{params[:tag_name]}").page(params[:page]).per(8).reverse_order
     end
   end
 
+  #日記詳細画面
   def show
     @diary = Diary.find(params[:id])
     @user = @diary.user
+    #日記内容一覧の表示（ページネーション）
     @diary_details = @diary.diary_details.page(params[:page]).per(10).reverse_order
   end
 
+  #日記編集画面
   def edit
     @diary = Diary.find(params[:id])
   end
 
+  #日記更新処理
   def update
     @diary = Diary.find(params[:id])
     @diary.user_id = current_user.id
@@ -44,12 +54,14 @@ class DiariesController < ApplicationController
     end
   end
 
+  #日記削除処理
   def destroy
     @diary = Diary.find(params[:id])
     @diary.destroy
     redirect_to user_path(current_user.id), notice: "日記が削除されました"
   end
 
+  #日記全削除処理
   def destroy_all
     @diaries = current_user.diaries
     @diaries.destroy_all
