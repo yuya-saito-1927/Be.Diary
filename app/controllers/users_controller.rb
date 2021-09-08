d70d5e8dbd5b84d2b504c3991136e7a4571b2b65
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-09-08 15:01:38 +0000

    Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,22 @@
 class UsersController < ApplicationController
+  
+  #ユーザ一覧画面
   def index
+    #ユーザ一覧のページネーション
     @users = User.all.page(params[:page]).per(10).reverse_order
     @user = current_user
     @diaries_side = @user.diaries.reverse_order
   end
 
+  #ユーザ詳細画面
   def show
     @user = User.find(params[:id])
+    #ユーザの日記のページネーション
     @diaries = @user.diaries.page(params[:page]).per(6).reverse_order
     @diaries_side = @user.diaries.reverse_order
   end
 
+  #ユーザ編集画面
   def edit
     @user = User.find(params[:id])
     if @user == current_user
@@ -20,6 +26,7 @@ class UsersController < ApplicationController
     end
   end
 
+  #ユーザ更新処理
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -29,10 +36,12 @@ class UsersController < ApplicationController
     end
   end
 
+  #ユーザ退会画面
   def unsubscribe
     @user = User.find(params[:id])
   end
 
+  #ユーザ退会処理
   def withdraw
     @user = User.find(params[:id])
     @user.update(is_deleted: true)
