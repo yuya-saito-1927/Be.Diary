712ce554fe2cdbce87c680535928c538a97d0e5d
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-09-08 14:49:07 +0000

    Update

--- app/controllers/diary_details_controller.rb
+++ app/controllers/diary_details_controller.rb
@@ -1,8 +1,11 @@
 class DiaryDetailsController < ApplicationController
+  
+  #日記内容作成画面
   def new
     @diary_detail = DiaryDetail.new
   end
 
+  #日記内容作成処理
   def create
     @diary = Diary.find(params[:diary_id])
     @diary_detail = DiaryDetail.new(diary_detail_params)
@@ -15,17 +18,20 @@ class DiaryDetailsController < ApplicationController
     end
   end
 
+  #日記内容詳細画面
   def show
     @diary = Diary.find(params[:diary_id])
     @diary_detail = DiaryDetail.find(params[:id])
     @post_comment = PostComment.new
   end
 
+  #日記内容編集画面
   def edit
     @diary = Diary.find(params[:diary_id])
     @diary_detail = DiaryDetail.find(params[:id])
   end
 
+  #日記内容更新処理
   def update
     @diary = Diary.find(params[:diary_id])
     @diary_detail = DiaryDetail.find(params[:id])
@@ -38,6 +44,7 @@ class DiaryDetailsController < ApplicationController
     end
   end
 
+  #日記内容削除
   def destroy
     @diary = Diary.find(params[:diary_id])
     @diary_detail = DiaryDetail.find(params[:id])
@@ -45,6 +52,8 @@ class DiaryDetailsController < ApplicationController
     @diary_detail.destroy
     redirect_to diary_path(@diary.id), notice: "日記内容が削除されました"
   end
+  
+  #日記内容全削除処理に関しては作成しませんでした。日記を削除した場合に全て内容も削除されるため。
 
   private
 
