acd87c012f9dd28cf7be6b9643ad741a0682752d
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-09-08 14:37:11 +0000

    Update

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,31 +1,39 @@
 class ContactsController < ApplicationController
   
+  #お問い合わせ入力画面
   def new
     @contact = Contact.new
   end 
   
+  #お問い合わせ内容確認画面
   def confirm
     @contact = Contact.new(contact_params)
+    #内容に不備があった場合作成画面に戻る
     if @contact.invalid?
       render :new
     end
   end
   
+  #お問い合わせ内容修正画面
   def back
     @contact = Contact.new(contact_params)
     render :new
   end
   
+  #お問い合わせ作成処理
   def create
     @contact = Contact.new(contact_params)
+    #作成処理完了の場合、メール送信
     if @contact.save
       ContactMailer.send_mail(@contact).deliver_now
       redirect_to done_path
+    #処理失敗の場合作成画面に
     else
       render :new
     end
   end
   
+  #お問い合わせ完了画面
   def done
   end
   
